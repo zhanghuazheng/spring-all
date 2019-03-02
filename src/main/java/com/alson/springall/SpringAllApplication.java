79b35b2ffc1228b82f54e38e6a1fc003efd1cdc3
zhanghuazheng <18911341753@163.com>
2019-03-02 11:41:39 +0800

    Initial commit

---
+++ src/main/java/com/alson/springall/SpringAllApplication.java
@@ -0,0 +1,13 @@
+package com.alson.springall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAllApplication.class, args);
+	}
+
+}
